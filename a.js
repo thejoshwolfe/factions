3ab09af0324f5a630c23855c154f0c001f31c505
Caleb Morris <caleb.morris.g@gmail.com>
2014-01-31 00:16:31 -0700

    Added multiple player selection with colorations

--- a.js
+++ a.js
@@ -9,18 +9,43 @@ factions.forEach(function(faction) {
 });
 var chosen = {};
 var originalFactionCount = factions.length;
+var playerCount = 1;
+var MAX_PLAYER_COUNT = 7;
 loadState();
 
 document.getElementById("generate").addEventListener("click", function() {
-  var chooseFrom = factions.filter(function(faction) { return included[faction] && !chosen[faction]; });
-  var faction = chooseFrom[Math.floor(Math.random() * chooseFrom.length)];
-  document.getElementById("faction").innerHTML = faction;
-  chosen[faction] = true;
-  generateList();
+  var chooseFrom = factions.filter(function(faction) { return included[faction] && !(faction in chosen); });
+  if (chooseFrom.length >= playerCount)
+  {
+    var chosenValues = [];
+    var factionTag = document.getElementById("faction");
+    var chosenValue = Math.floor(Math.random() * chooseFrom.length);
+    var faction = chooseFrom[chosenValue];
+    factionTag.innerHTML = faction;
+    chosenValues.push(chosenValue);
+    chosen[faction] = 1;
+    for (var i = playerCount; i > 1; i--) {
+      chosenValue = Math.floor(Math.random() * chooseFrom.length);
+      while(chosenValues.indexOf(chosenValue) > -1)
+      {
+        chosenValue = Math.floor(Math.random() * chooseFrom.length);
+      }
+      chosenValues.push(chosenValue);
+      faction = chooseFrom[chosenValue];
+      factionTag.innerHTML += ", " + faction;
+      chosen[faction] = i;
+    };
+    generateList();
+  }
+  else
+  {
+    document.getElementById("faction").innerHTML = "Not enough factions for the number of players";
+  }
 });
 document.getElementById("start_over").addEventListener("click", function() {
   chosen = {};
   document.getElementById("faction").innerHTML = "?";
+  updatePlayerCountLabel(0);
   generateList();
 });
 
@@ -28,8 +53,8 @@ generateList();
 function generateList() {
   document.getElementById("faction_list").innerHTML = factions.map(function(faction, i) {
     return '<li>' +
-      '<label'+(chosen[faction]?' class="chosen"':'')+'>' +
-        '<input type="checkbox" id="faction_'+i+'"'+(included[faction]?' checked="true"':'')+'>' +
+      '<label'+((chosen[faction] > 0)?' class="chosen' + chosen[faction] + '"':'')+'>' +
+        '<input type="checkbox" id="faction_'+i+'"'+((included[faction])?' checked="true"':'')+'>' +
           faction +
         '</label>' +
       (i >= originalFactionCount ? '<button id="remove_faction_'+i+'">x</button>' : '') +
@@ -88,6 +113,35 @@ function addNewFaction() {
   }
   newFactionTextbox.focus();
 };
+
+var playerCounter = document.getElementById("number_of_players");
+updatePlayerCountLabel(playerCount);
+
+document.getElementById("increment_players").addEventListener("click", function() {
+  updatePlayerCountLabel(++playerCount);
+});
+
+document.getElementById("decrement_players").addEventListener("click", function() {
+  updatePlayerCountLabel(--playerCount);
+});
+
+function updatePlayerCountLabel(count)
+{
+  if (count < 1)
+  {
+    playerCount = 1;
+    playerCounter.innerHTML = playerCount;
+  }
+  else if (count > MAX_PLAYER_COUNT)
+  {
+    playerCount = MAX_PLAYER_COUNT;
+    playerCounter.innerHTML = playerCount;
+  }
+  else
+  {
+    playerCounter.innerHTML = count;
+  }
+}
 
 function saveState() {
   localStorage.factions = JSON.stringify({
